b134a17a1ef1639645a0310fd6523a052648776e
Mitchell <mwhyuen@gmail.com>
2021-12-23 15:59:04 +0200

    feat: add header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import { Provider } from 'react-redux';
 
 import './App.scss';
 import store from './redux/store';
-
+import Header from './components/header/Header';
 function App() {
   return (
     <Provider store={store}>
+      <Header> </Header>
       <div className="app">Redux</div>
     </Provider>
   );
